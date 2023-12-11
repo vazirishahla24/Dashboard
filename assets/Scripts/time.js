288b8e1b96e2a8aa4c4b52dd3d65479344c433d8
vazirishahla24 <vaziri.shahla24@gmail.com>
2023-12-11 21:45:13 +0330

    final

--- assets/Scripts/time.js
+++ assets/Scripts/time.js
@@ -4,7 +4,6 @@ const fetchTime = async() => {
     try {
         const response = await fetch(`https://kaaryar0506reactblog.liara.run/current/time`);
         const data = await response.json();
-        format(2);
         return data;
 
     } catch (error) {
@@ -23,8 +22,6 @@ times.then((e) => {
         month: 'short',
         day: '2-digit',
     };
-
-    console.log(e);
 
     var ghamari = date.toLocaleDateString('ar-TN-u-ca-islamic', options);
     ghamari = ghamari.replace('هـ', '');
@@ -50,4 +47,53 @@ times.then((e) => {
                     <option value="">تایمر</option>
                 </select>
             </div>`;
-})
+});
+
+
+// const fetchTime = () => {
+//     fetch(`https://kaaryar0506reactblog.liara.run/current/time`)
+//         .then((res) => {
+//             return res.json();
+//         })
+//         .then((data) => {
+//             const options = {
+//                 year: 'numeric',
+//                 month: 'short',
+//                 day: '2-digit',
+//             };
+//             var date = new Date(data.current);
+
+//             var ghamari = date.toLocaleDateString('ar-TN-u-ca-islamic', options);
+//             ghamari = ghamari.replace('هـ', '');
+//             ghamari = ghamari.replace(' ', '/');
+
+
+//             var miladi = date.toLocaleDateString('en-CA', options);
+//             miladi = miladi.replace(',', '/');
+//             miladi = miladi.replace(' ', '/');
+
+//             time.innerHTML = "";
+//             time.innerHTML +=
+//                 `<h1>${date.getHours().toLocaleString('fa-ir')}:${date.getMinutes().toLocaleString('fa-ir')}</h1>
+//             <h3 class="direction-rtl">${data.shamsi.dayInMonth} ${data.shamsi.month}</h3>
+//             <p class="d-flex">
+//                 <span>${miladi}</span>
+//                 <span>${ghamari}</span>
+//             </p>
+//             <div>
+//                 <select class="direction-rtl">
+//                     <option value="">اوقات شرعی</option>
+//                 </select>
+//                 <select class="direction-rtl">
+//                     <option value="">تایمر</option>
+//                 </select>
+//             </div>`
+
+//         })
+//         .catch((error) => {
+//             console.log(error);
+//         })
+
+//     setTimeout(fetchTime(), 60000);
+// };
+// fetchTime();
